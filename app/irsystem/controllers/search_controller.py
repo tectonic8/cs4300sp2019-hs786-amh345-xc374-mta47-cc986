450924390fc11d34a1c156a174a01f78b13c7b54
Morgan Aloia <mta47@cornell.edu>
2019-04-15 18:32:49 -0400

    added authors

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -41,13 +41,17 @@ def search():
 	                                      idf="log"
 	                                     )
 			
-		# data[i][0] = Title, data[i][1] = SimScore, data[i][2] = (Trope, RelScore)
+		# data[i][0] = Title, data[i][1] = Author, data[i][2] = SimScore, data[i][3] = (Trope, RelScore)
 		if retrieval:
 			invQ = False
 			i = 0
 			for entry in retrieval[0]:
 				data.append([])
 				data[i].append(entry[0])
+				if "author" in booksJSON[entry[0].lower()]:
+					data[i].append(booksJSON[entry[0].lower()]["author"])
+				else:
+					data[i].append("Not Listed")
 				data[i].append(entry[1])
 				data[i].append(retrieval[1][entry[0]])
 				i += 1
