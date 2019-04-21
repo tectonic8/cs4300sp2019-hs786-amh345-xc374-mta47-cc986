b00831818527a123b11d870a8b3dcb7f937585da
Morgan Aloia <mta47@cornell.edu>
2019-04-20 21:08:10 -0400

    full page carding, first implementation

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -16,9 +16,14 @@ def search():
 	else:
 		outputType = "movie"
 
-	query = None
+	q = None
 	if(request.args.get('query')):
-		query = request.args.get('query')
+		q = request.args.get('query')
+	
+	if(not request.args.get('spec')):
+		spec = "False"
+	else:
+		spec = dict()
 
 	outputMessage = None
 	output = []
@@ -26,9 +31,9 @@ def search():
 	inspiration = None
 
 	# initialize internal vars
-	with open('app/irsystem/controllers/DatasetInfo/tbwb_book_dataset.json') as json_file:
+	with open('app/irsystem/controllers/DatasetInfo/book_dataset.json') as json_file:
 	    booksJSON = json.load(json_file)
-	with open('app/irsystem/controllers/DatasetInfo/tbwb_movie_dataset.json') as json_file:
+	with open('app/irsystem/controllers/DatasetInfo/movie_dataset.json') as json_file:
 	    moviesJSON = json.load(json_file)
 
 	for book in books:
@@ -37,15 +42,14 @@ def search():
 	for movie in movies:
 		if(movie.lower() in moviesJSON):
 			moviesJSON[movie] = moviesJSON.pop(movie.lower())
-
 	retrieval = None
 
 	# run query
-	if(query):
-		outputMessage = "Your Search: " + query
+	if(q):
+		outputMessage = "Your Search: " + q
 		retrieval = find_relevant(datasets = datasets,
                                   inverted_indices = inverted_indices,
-                                  query = query, 
+                                  query = q, 
                                   input_category = queryType,
                                   result_category = outputType,
                                   min_df = 3,
@@ -55,11 +59,11 @@ def search():
 	# set export vars
 	validQueries = ""
 	if(queryType == "movie"):
-		for query in movies:
-			validQueries += query + ", "
+		for m in movies:
+			validQueries += m + ", "
 	else:
-		for query in books:
-			validQueries += query + ", "
+		for b in books:
+			validQueries += b + ", "
 	validQueries = validQueries[:-2]
 
 	if(not request.args.get('query')):
@@ -71,26 +75,60 @@ def search():
 		inspiration = randomNInsp(moviesJSON, 10)
 	if(queryType == "book"):
 		inspiration = randomNInsp(booksJSON, 10)
-
-	if retrieval:
-		i = 0
-		for entry in retrieval[0]:
-			output.append(dict())
-			if(queryType == "movie"):
-				output[i]["title"] = entry[0]
-				if "author" in booksJSON[entry[0]]:
-					output[i]["author"] = booksJSON[entry[0]]["author"]
-				else:
-					output[i]["author"] = "Author Not Listed"
-				output[i]["simScore"] = entry[1]
-				output[i]["tropes"] = "".join(elem for elem in topNTropes(retrieval[1][entry[0]], 5))
-			if(queryType == "book"):
-				output[i]["title"] = entry[0]
-				output[i]["simScore"] = entry[1]
-				output[i]["tropes"] = "".join(elem for elem in topNTropes(retrieval[1][entry[0]], 5))
-			i += 1
-	elif(query):
-		outputMessage = "Sorry, \'" + query + "\' is an invalid query."
+	if spec == "False":
+		if retrieval:
+			i = 0
+			for entry in retrieval[0]:
+				output.append(dict())
+				if(queryType == "movie"):
+					output[i]["title"] = entry[0]
+					if "author" in booksJSON[entry[0]]:
+						output[i]["author"] = booksJSON[entry[0]]["author"]
+					output[i]["simScore"] = round(entry[1], 3)
+					output[i]["tropes"] = "".join(elem for elem in topNTropes(retrieval[1][entry[0]], 5))
+					if "reviews" in booksJSON[entry[0]]:
+						output[i]["reviews"] = list()
+						for review in booksJSON[entry[0]]["reviews"]:
+							output[i]["reviews"].append(review)
+					if "img" in booksJSON[entry[0]]:
+						output[i]["img"] = booksJSON[entry[0]]["img"]
+				if(queryType == "book"):
+					output[i]["title"] = entry[0]
+					output[i]["simScore"] = round(entry[1], 3)
+					output[i]["tropes"] = "".join(elem for elem in topNTropes(retrieval[1][entry[0]], 5))
+					if entry[0] in moviesJSON and "img" in moviesJSON[entry[0]]:
+						output[i]["img"] = moviesJSON[entry[0]]["img"]
+				
+				i += 1
+		elif(q):
+			outputMessage = "Sorry, \'" + q + "\' is an invalid query."
+	else:
+		if(queryType == "movie"):
+			spec["title"] = q
+			if "author" in booksJSON[q]:
+				spec["author"] = booksJSON[q]["author"]
+			if "rating" in booksJSON[q]:
+				spec["rating"] = booksJSON[q]["rating"]
+			if "published" in booksJSON[q]:
+				spec["year"] = booksJSON[q]["published"]
+			if "summary" in booksJSON[q]:
+				spec["summary"] = booksJSON[q]["summary"]
+			if "reviews" in booksJSON[q]:
+				spec["reviews"] = booksJSON[q]["reviews"]
+			spec["tropes"] = allTropes(book_tropes_data[q])
+		else:
+			spec["title"] = q
+			if q in moviesJSON:
+				if "rating" in moviesJSON[q]:
+					spec["rating"] = moviesJSON[q]["rating"]
+				if "published" in moviesJSON[q]:
+					spec["year"] = moviesJSON[q]["published"]
+				if "summary" in moviesJSON[q]:
+					spec["summary"] = moviesJSON[q]["summary"]
+				if "reviews" in moviesJSON[q]:
+					spec["reviews"] = "THIS IS TEMPORARY"
+			spec["tropes"] = allTropes(movie_tropes_data[q])
+			
 
 	# export
-	return render_template('search.html', isHomeScreen = isHomeScreen, inspiration = inspiration, validQueries = validQueries, queryType = queryType, query = query, outputMessage = outputMessage, outputType = outputType , output = output)
+	return render_template('search.html', isHomeScreen = isHomeScreen, inspiration = inspiration, validQueries = validQueries, queryType = queryType, query = q, outputMessage = outputMessage, outputType = outputType , output = output, spec = spec)
