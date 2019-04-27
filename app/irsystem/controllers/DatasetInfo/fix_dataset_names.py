3a6fa2794a4b708e7ddc2a05ec89c5ea5ad4f9fd
Caleb Chiam <calebchiam@gmail.com>
2019-04-27 16:29:40 -0400

    fixing dataset titles + reindexing

--- app/irsystem/controllers/DatasetInfo/fix_dataset_names.py
+++ app/irsystem/controllers/DatasetInfo/fix_dataset_names.py
@@ -1,19 +1,52 @@
 import json
 
-with open("movie_dataset.json", 'r') as f:
-    movie_dataset = json.load(f)
+dataset_type = "movie"
 
-with open("../TVTropesScraper/Film/Film_tropes_dataset3.json", 'r') as f:
-    movie_tropes_dataset = json.load(f)
+if dataset_type == "movie":
 
-lower_to_proper = {title.lower(): title for title in movie_tropes_dataset}
+    with open("movie_dataset.json", 'r') as f:
+        movie_dataset = json.load(f)
 
-movie_dataset2 = dict()
-for title in movie_dataset:
-    if title.lower() in lower_to_proper:
-        movie_dataset2[lower_to_proper[title.lower()]] = movie_dataset[title]
-    else:
-        print("Could not match {}".format(title))
+    with open("../TVTropesScraper/Film/Film_tropes_dataset3.json", 'r') as f:
+        movie_tropes_dataset = json.load(f)
 
-with open("movie_dataset.json", 'w') as f:
-    json.dump(movie_dataset2, f)
+    lower_to_proper = {title.lower(): title for title in movie_tropes_dataset}
+
+    movie_dataset2 = dict()
+    for title in movie_dataset:
+        if title.lower() in lower_to_proper:
+            movie_dataset2[lower_to_proper[title.lower()]] = movie_dataset[title]
+        else:
+            print("Could not match {}".format(title))
+
+    idx = 0
+    for k in movie_dataset2:
+        movie_dataset2[k]["idx"] = idx
+        idx += 1
+    print(idx)
+
+    with open("movie_dataset2.json", 'w') as f:
+        json.dump(movie_dataset2, f)
+else:
+    with open("book_dataset.json", 'r') as f:
+        book_dataset = json.load(f)
+
+    with open("../TVTropesScraper/Literature/Literature_tropes_dataset3.json", 'r') as f:
+        book_tropes_dataset = json.load(f)
+
+    lower_to_proper = {title.lower(): title for title in book_tropes_dataset}
+
+    book_dataset2 = dict()
+    for title in book_dataset:
+        if title.lower() in lower_to_proper:
+            book_dataset2[lower_to_proper[title.lower()]] = book_dataset[title]
+        else:
+            print("Could not match {}".format(title))
+
+    idx = 0
+    for k in book_dataset2:
+        book_dataset2[k]["idx"] = idx
+        idx += 1
+    print(idx)
+    with open("book_dataset.json", 'w') as f:
+        json.dump(book_dataset2, f)
